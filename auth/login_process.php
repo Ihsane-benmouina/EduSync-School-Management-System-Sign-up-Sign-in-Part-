5752f8d03c5a2270809a0a0d11f5090f2f60aafd
Ihsane-12 <benminaihssane@gmail.com>
2026-04-22 12:16:37 +0100

    ajout de la vérification des identifiants et redirection après connexion

--- auth/login_process.php
+++ auth/login_process.php
@@ -8,7 +8,17 @@ if($_SERVER["REQUEST_METHOD"=="POST"]){
     $stmt=$conn->prepare($sql);
     $stmt->execute(['email'=>$email]);
     $user= $stmt->fetch();
+    
 
+    if($user && password_verify($password,$user['passwork']))
+        $_SESSION['user_id']=$user['id'];
+    header("Location:../dashboard/index.php");
+    exit();
+}else{
+    echo"email ou mot de passe  incorrect";
+
+
+}
     
 
 

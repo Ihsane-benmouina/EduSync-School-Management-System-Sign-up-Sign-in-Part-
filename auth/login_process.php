197c60592d4a79fb5f7d3ba8adc29e465a08aede
Ihsane-12 <benminaihssane@gmail.com>
2026-04-22 12:17:07 +0100

    Refactor login process for improved readability and security

--- auth/login_process.php
+++ auth/login_process.php
@@ -1,34 +1,26 @@
-<?php 
+<?php
 require '../config/db.php';
-if($_SERVER["REQUEST_METHOD"=="POST"]){
-    $email= htmlspecialchars($_POST['email']);
-    $password=$_POST['password'];
+session_start();
 
-    $sql="SELECT * FROM users WHERE $email=:email";
-    $stmt=$conn->prepare($sql);
-    $stmt->execute(['email'=>$email]);
-    $user= $stmt->fetch();
-    
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    if($user && password_verify($password,$user['passwork']))
-        $_SESSION['user_id']=$user['id'];
-    header("Location:../dashboard/index.php");
-    exit();
-}else{
-    echo"email ou mot de passe  incorrect";
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
 
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(['email' => $email]);
 
+    $user = $stmt->fetch();
+
+    if($user && password_verify($password, $user['passwork'])){
+        
+        $_SESSION['user_id'] = $user['id'];
+
+        header("Location: ../dashboard/index.php");
+        exit();
+    } else {
+        echo "Email ou mot de passe incorrect";
+    }
 }
-    
-
-
-
-
-};
-
-
-
-
-
-
 ?>

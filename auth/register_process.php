6d54b9f0dc332c5a013c4735cdaad9741aaecc1c
Ihsane-12 <benminaihssane@gmail.com>
2026-04-22 12:37:16 +0100

    Fix registration process logic and improve code formatting

--- auth/register_process.php
+++ auth/register_process.php
@@ -1,8 +1,6 @@
-<?php
-require '../config/db.php';
+<?php require '../config/db.php';
 session_start();
-
-if($_SERVER["REQUEST_METHOD"]="POST"){
+if($_SERVER["REQUEST_METHOD"]=="POST"){
     $firstName=$_POST['firstName'];
     $lastName= trim($_POST['lastName']);
     $email= htmlspecialchars(trim($_POST['email']));
@@ -10,42 +8,47 @@ if($_SERVER["REQUEST_METHOD"]="POST"){
     $confirmPassword=$_POST['confirmPassword'];
 
 
-
-    if ($password !== $confirmPassword) {
+if ($password !== $confirmPassword) {
     echo "Les mots de passe ne correspondent pas!";
     exit();
 }
 
-$hashpassword=password_hash($password,PASSWORD_DEFAULT);
+    $hashedpassword=password_hash($password, PASSWORD_DEFAULT);
+    $sql=" SELECT * FROM users WHERE email=:email";
+    $stmt= $conn->prepare($sql);
+    $stmt->execute(['email'=>$email]);
+    $user=$stmt->fetch();
+
+    if($user){
+        echo"Email deja utilise";
+        exit();
+    }
+
+    $role = 3;
+    $sql="INSERT INTO users(`firstName`,`lasttName`,`email`,`passwork`,`id_role`) 
+    VALUES(:prenom, :lastName, :email, :password,:id_role)";
+    $stmt=$conn->prepare($sql);
+    $stmt->execute([
+        'prenom'=>$firstName,
+        'lastName'=>$lastName,
+        'email'=>$email,
+        'password'=>$hashedpassword,
+        'id_role'=>$role,
+    ]);
+
+$_SESSION['user_id'] = $conn->lastInsertId();
 
 
-$sql="SELECT $ FROM users WHERE email=:email";
-$stmt=$conn->prepare($sql);
-$stmt->execute(['email'=>$email]);
-$user=$stmt->fetch();
-
-if($user){
-    echo"email deja exict";
-}
-
-$role=3;
-$sql="INSERT INTO users(`firstName`,`lasttName`,`email`,`passwork`,`id_role`)  
-VALUES(:nom ,:prenom ,:email , :password , :role )";
-$stmt=$conn->prepare($sql);
-$stmt->execute([
-    'nom'=>$firstName,
-    'prenom'=>$lastName,
-    'email'=>$email,
-    'password'=>$hashpassword,
-    'role'=>$role,
-]);
-
-$_SESSION['user_id']=$conn->lastInsertId();
 header("Location: ../dashboard/index.php");
 exit();
 
 
-}
+
+
+    
+
+
+};
 
 
 ?>

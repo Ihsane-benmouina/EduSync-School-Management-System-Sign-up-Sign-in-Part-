2b830fca1a15a45b733ea315a48dd3cc36b53802
Ihsane-12 <benminaihssane@gmail.com>
2026-04-23 14:41:23 +0100

    Refactor registration process for improved readability and maintainability

--- auth/register_process.php
+++ auth/register_process.php
@@ -1,25 +1,14 @@
 <?php require '../config/db.php';
 session_start();
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $firstName=$_POST['firstName'];
-    $lastName= trim($_POST['lastName']);
-    $email= htmlspecialchars(trim($_POST['email']));
-    $password=$_POST['password'];
-    $confirmPassword=$_POST['confirmPassword'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = htmlspecialchars($_POST['firstName']);
+    $lastName = htmlspecialchars($_POST['lastName']);
+    $email = htmlspecialchars(trim($_POST['email']));
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
 
 
-if ($password !== $confirmPassword) {
-    echo "Les mots de passe ne correspondent pas!";
-    exit();
-}
-
-    $hashedpassword=password_hash($password, PASSWORD_DEFAULT);
-    $sql=" SELECT * FROM users WHERE email=:email";
-    $stmt= $conn->prepare($sql);
-    $stmt->execute(['email'=>$email]);
-    $user=$stmt->fetch();
-
-      if (empty($firstName) || empty($lastName) || empty($email) || empty($password) || empty($confirmPassword)) {
+    if (empty($firstName) || empty($lastName) || empty($email) || empty($password) || empty($confirmPassword)) {
         header("Location:register.php?error=remplir tous les champs");
         exit();
     }
@@ -33,32 +22,33 @@ if ($password !== $confirmPassword) {
         exit();
     }
 
+    $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = " SELECT * FROM users WHERE email=:email";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(['email' => $email]);
+    $user = $stmt->fetch();
+
+    if ($user) {
+        header("Location:register.php?error=email deja exict ");
+        exit();
+    }
 
     $role = 3;
-    $sql="INSERT INTO users(`firstName`,`lasttName`,`email`,`passwork`,`id_role`) 
+    $sql = "INSERT INTO users(`firstName`,`lasttName`,`email`,`passwork`,`id_role`) 
     VALUES(:prenom, :lastName, :email, :password,:id_role)";
-    $stmt=$conn->prepare($sql);
+    $stmt = $conn->prepare($sql);
     $stmt->execute([
-        'prenom'=>$firstName,
-        'lastName'=>$lastName,
-        'email'=>$email,
-        'password'=>$hashedpassword,
-        'id_role'=>$role,
+        'prenom' => $firstName,
+        'lastName' => $lastName,
+        'email' => $email,
+        'password' => $hashedpassword,
+        'id_role' => $role,
     ]);
 
-$_SESSION['user_id'] = $conn->lastInsertId();
-
-
-header("Location: ../dashboard/index.php");
-exit();
 
 
 
 
-    
-
-
+    header("Location: login.php");
+    exit();
 };
-
-
-?>
